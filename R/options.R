9b96b939cc03272bc26016d92d8142910fda6cc3
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-09-16 12:55:56 +0000

    put options in a separated file

---
+++ R/options.R
@@ -0,0 +1,32 @@
+#' Set an option for tramoseats
+#'
+#' @param name Name of the option
+#' @param obj Option
+#'
+#' @returns Invisibly `NULL`
+#'
+#' @export
+#'
+#' @examples
+#' tramoseats_option("test", "DUMMY")
+tramoseats_option <- function(name, obj) {
+    options <- rjd3toolkit::.jd3_env$tramoseats
+    options[[name]] <- obj
+    assign("tramoseats", options, rjd3toolkit::.jd3_env)
+    return(invisible(NULL))
+}
+
+#' Set an option for tramoseats
+#'
+#' @param name Name of the option
+#'
+#' @returns The requested option or NULL if it doesn't exist
+#' @export
+#'
+#' @examples
+#' tramoseats_option("test", "DUMMY")
+#' get_tramoseats_option("test")
+get_tramoseats_option <- function(name) {
+    options <- rjd3toolkit::.jd3_env$tramoseats
+    return(options[[name]])
+}
--- R/zzz.R
+++ R/zzz.R
@@ -55,34 +55,3 @@ NULL
         options(summary_info = TRUE)
     }
 }
-
-#' Set an option for tramoseats
-#'
-#' @param name Name of the option
-#' @param obj Option
-#'
-#' @export
-#'
-#' @examples
-#' tramoseats_option("test", "DUMMY")
-tramoseats_option <- function(name, obj) {
-    options <- rjd3toolkit::.jd3_env$tramoseats
-    options[[name]] <- obj
-    assign("tramoseats", options, rjd3toolkit::.jd3_env)
-    invisible()
-}
-
-#' Set an option for tramoseats
-#'
-#' @param name Name of the option
-#'
-#' @returns The requested option or NULL if it doesn't exist
-#' @export
-#'
-#' @examples
-#' tramoseats_option("test", "DUMMY")
-#' get_tramoseats_option("test")
-get_tramoseats_option <- function(name) {
-    options <- rjd3toolkit::.jd3_env$tramoseats
-    return(options[[name]])
-}
